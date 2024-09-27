e95af343bb749ec61c8619286e5776146053387d
Rebecca Nolan <G00423921@atu.ie>
2024-09-27 11:19:21 +0000

    3 completed

---
+++ 3.js
@@ -0,0 +1,15 @@
+let Tasks = [];
+
+//b Tasks.push(value);
+let addTask = (task) => {
+    Tasks.push(task);
+    console.log(task + " had been added to my tasks.")
+}
+
+
+
+//c Tasks.forEach(()=>{});
+
+
+//d let index = Tasks.indexOf(value);
+//d Tasks.splice(index,1);
