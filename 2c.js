e1ff183816a1d6cc541c9e859bf72ce94af18c60
Rebecca Nolan <127899503+RebeccaNolan@users.noreply.github.com>
2024-09-28 23:59:56 +0100

    Update 2c.js
    Updated because comments got deleted.

--- 2c.js
+++ 2c.js
@@ -1,4 +1,7 @@
+//Function that takes in two parameters
 let c = (num1, num2) => {
+    //returns the num of the numbers
     return num1 + num2;
 }
+//Assigns the numbers and prints them
 console.log(c(10, 10));
