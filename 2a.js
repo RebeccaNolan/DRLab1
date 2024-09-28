b4fcad9f3e97100325a6f6ca075a361b5096066e
Rebecca Nolan <127899503+RebeccaNolan@users.noreply.github.com>
2024-09-28 23:57:54 +0100

    Update 2a.js
    Comments got deleted from last push, after dealing with errors and different branches, I think.

--- 2a.js
+++ 2a.js
@@ -1,36 +1,6 @@
+//Arrow function to return a line of text
 let a = () => {
     return "Data Representation & Querying";
 }
+//Output a
 console.log(a());
-
-/*function Hello() {
-    console.log("hello world!");
-}
-//invoke function
-Hello();
-
-let a = function Hello() {
-    console.log("hello");
-}
-
-a();
-
-let b = () => {
-    console.log("hello b");
-}
-b();
-
-let c = (value) => {
-    console.log(value);
-}
-c("bye");
-*/
-
-//git init - make empty repo in folder
-//git add 2a.js (will ask who  you are) git config --global username
-///git commit -m "comment here"
-
-//copy paste 
-//git branch -M main
-//git remote add origin https://github.com/RebeccaNolan/DRLab1.git
-//git push -u origin main
