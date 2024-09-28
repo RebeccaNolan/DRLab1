c133df1b9737c8589bb8ce39ffea3a079c8cba9f
Rebecca Nolan <127899503+RebeccaNolan@users.noreply.github.com>
2024-09-29 00:01:44 +0100

    Update 3.js

--- 3.js
+++ 3.js
@@ -17,19 +17,25 @@ let listAllTasks = () => {
 //3d. deleteTask function
 let deleteTask = (task) => {
     let index = Tasks.indexOf(task);
-    
+
+    //if greater than 1, it's in the array
     if(index > -1) {
         Tasks.splice(index, 1);
         console.log(task + " has been deleted from my tasks");
     }else {
         console.log(task + " not found");
-    }//didnt find in array
+    }//if not found in array
 
     return Tasks.length;
 }
 
+//add tasks to the array
 addTask("Task 1");
 addTask("Task 2");
 addTask("Task 3");
+
+//list all of them
 listAllTasks();
+
+//delete a specific task
 deleteTask("Task 2");
